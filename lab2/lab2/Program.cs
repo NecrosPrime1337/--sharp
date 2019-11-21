2b0da0666f71f68031cc7ba03084a110276cd73e
NecrosPrime1337 <57898827+NecrosPrime1337@users.noreply.github.com>
2019-11-21 19:24:59 +0300

    Add files via upload

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 namespace ConsoleApplication1
 {
-    class Program
+    internal class Program
     {
+
+       
         static void V1(string[] args)
+
         {
             Console.Clear();
             Console.WriteLine("Выполнение варианта 1");
             Console.WriteLine("Определить время, через которое встретятся два тела, равноускоренно движущиеся навстречу друг другу,");
             Console.WriteLine("если известны их начальные скорости, ускорения и начальное расстояние между ними.");
-            double sp1, sp2, a1, a2, leng, t ;
+            double sp1, sp2, a1, a2, leng, t;
             Console.WriteLine("ожидание ввода скорости первого Sp1=");
             sp1 = double.Parse(Console.ReadLine());
             Console.WriteLine("ожидание ввода ускорения первого а1=");                      //
@@ -38,7 +42,7 @@ namespace ConsoleApplication1
             b = double.Parse(Console.ReadLine());
             Console.WriteLine("ожидание ввода угла при большей стороне (в радианах) c=");
             c = double.Parse(Console.ReadLine());
-            s = (Math.Pow(a,2)-Math.Pow(b,2)) / 2 * (Math.Tan(c));
+            s = ((Math.Pow(a, 2) - Math.Pow(b, 2)) / 2 )* (Math.Tan(c));
             Console.WriteLine("Площадь  s={0}", s);
             Console.ReadKey();
             Main(args); // возврат в главное меню
@@ -66,22 +70,22 @@ namespace ConsoleApplication1
             c = double.Parse(Console.ReadLine());
             if (a < b + c & b < a + c & c < a + b)
             {
-                p = (a + b + c) / 2;
-                ha = (2 / a) * Math.Sqrt(p * (p - a) * (p - b) * (p - c)); 
+                p = (a + b + c) / 2;  //полупериметр
+                ha = (2 / a) * Math.Sqrt(p * (p - a) * (p - b) * (p - c));
                 hb = (2 / b) * Math.Sqrt(p * (p - a) * (p - b) * (p - c));
                 hc = (2 / c) * Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-            //=======================================================
-                ma = 2 * (Math.Sqrt((b*b + c*c) - a*a));
-                mb = 2 * (Math.Sqrt((a*a + c*c) - b*b));
-                mc = 2 * (Math.Sqrt((a*a + b*b) - c*c));
-            //=======================================================
-                s= Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                //=======================================================
+                ma = 2 * (Math.Sqrt((b * b + c * c) - a * a));
+                mb = 2 * (Math.Sqrt((a * a + c * c) - b * b));
+                mc = 2 * (Math.Sqrt((a * a + b * b) - c * c));
+                //=======================================================
+                s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
                 rb = a * b * c / (4 * s);
                 rm = s / p;
                 sm = pi * (rm * rm);
                 sb = pi * (rb * rb);
                 Console.WriteLine("длины высот: ha={0} hb={0} hc={0}", ha, hb, hc);
-                Console.WriteLine("длины медиан: ma={0} mb={0} mc={0}",ma,mb,mc );
+                Console.WriteLine("длины медиан: ma={0} mb={0} mc={0}", ma, mb, mc);
                 Console.WriteLine("радиусы вписанной и описанной окружностей: rm={0} rb={0}", rm, rb);
                 Console.WriteLine("площади: sm={0} sb={0}", sm, sb);
                 Console.ReadKey();
@@ -90,19 +94,36 @@ namespace ConsoleApplication1
             else
             {
                 Console.WriteLine("Это не треугольник. Продолжить?");
+                char ch;
+                ch = char.Parse(Console.ReadLine());
+                if (ch == 'y')
+                {
+                    V3(args);
+                }
+                else
+                {
+                    Main(args);
+                }
             }
-            sm = (Math.Pow(a, 2) - Math.Pow(b, 2)) / 2 * (Math.Tan(c));
-            Console.WriteLine("Площадь  s={0}", sm);
+
         }
 
         static void V8(string[] args)
         {
-            Console.WriteLine();
+            Console.Clear();
+            Console.WriteLine("вариант 8");
+            Console.WriteLine("натуральное число X. вычислить формулу. разрешено использовать только 4 умножения, ваычитания, и сложения");  //это хардовоооо
+            Console.WriteLine("значение X = ");
+            double x, y;
+            x = double.Parse(Console.ReadLine());
+            y = (((2 * x - 3) * x + 4) * x - 5) * x + 6; //2x^4-3x^3+4x^2-5x+6
+            Console.WriteLine("ответ y={0}", y);
         }
 
         static void V16(string[] args)
         {
             double x1, x2, x3, rad;
+            Console.Clear();
             Console.Write("Введите значение радиуса rad=");
             rad = double.Parse(Console.ReadLine());
 
@@ -115,68 +136,91 @@ namespace ConsoleApplication1
             Console.WriteLine("объем шара x3={0}", x3);
             Console.Write("Нажмите любую клавишу для продолжения . . . ");
             Console.ReadKey(true);
+        }
+        static string[] colors = Enum.GetNames(typeof(ConsoleColor));
+        static int indexTx = 0;
+        static int indexBg = 15;
+        void colored(string[] args)
+        {
+
+        }
+
+
+        static void CallBack(object state)
+        {
+            Encoding enc = Encoding.GetEncoding(20127);
+            Encoding altEnc = Encoding.GetEncoding("ASCII");
+            Console.OutputEncoding = Encoding.Unicode;
+            Random rndNum = new Random(int.Parse(Guid.NewGuid().ToString().Substring(0, 8), System.Globalization.NumberStyles.HexNumber));
+            int rnd1 = rndNum.Next(0, 15);
+            indexTx = rnd1;
+            int rnd2 = rndNum.Next(0, 15);
+            indexBg = rnd2;
+            indexBg = rnd2;
+            indexTx = (indexTx == colors.Length) ? 0 : indexTx;
+            indexBg = (indexBg == colors.Length) ? 0 : indexBg;
+            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[indexBg]);
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[indexTx]);
+            //Очистка экрана
+            Console.Clear();
+            Console.WriteLine("================================================================================");
+            Console.WriteLine("||                                                                            ||");
+            Console.WriteLine("||         ███   ████   █  █   ███   ███                                      ||");
+            Console.WriteLine("||           █   █  █   █  █   █      █                                       ||");
+            Console.WriteLine("||         ███   ████   ████   ███    █                                       ||");
+            Console.WriteLine("||           █   █  █      █   █      █   █                                   ||");
+            Console.WriteLine("||         ███   █  █      █   ███    █   █                                   ||");
+            Console.WriteLine("||                                                                            ||");
+            Console.WriteLine("||         ████   ████   █ █ █   ████     ██   ████   ███   ████   █          ||");
+            Console.WriteLine("||         █  █   █  █   █████   █  █    █ █   █  █    █    █  █   █          ||");
+            Console.WriteLine("||         █  █   █  █    ███    ████   █  █   █       █    ████   █          ||");
+            Console.WriteLine("||         █  █   █  █   █████   █  █   █  █   █  █    █    █  █              ||");
+            Console.WriteLine("||         █  █   ████   █ █ █   █  █   █  █   ████    █    █  █   █          ||");
+            Console.WriteLine("||                                                                            ||");
+            Console.WriteLine("================================================================================");
+
         }
         static void autor(string[] args)
         {
-            double x1, x2, x3, rad;
-            Console.Write("Введите значение радиуса rad=");
-            rad = double.Parse(Console.ReadLine());
 
-            const double pi = 3.1415;
-            x1 = 2 * pi * rad;
-            x2 = rad * rad * pi;
-            x3 = pi * 0.75 * (rad * rad * rad); //можно использовать функцию Math.Pow(rad,3) для возведения в куб, но по заданию сказано низя :( 
-            Console.WriteLine("длина окружности x1={0}", x1);
-            Console.WriteLine("площадь круга x2={0}", x2);
-            Console.WriteLine("объем шара x3={0}", x3);
-            Console.Write("Нажмите любую клавишу для продолжения . . . ");
-            Console.ReadKey(true);
-        }
-        static void leet(string[] args)
-        {
-            Encoding enc = Encoding.GetEncoding(20127);
-            Encoding altEnc = Encoding.GetEncoding("US-ASCII");
-            Console.OutputEncoding = Encoding.ASCII;
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⢴⡶⣶⣶⣶⡒⣶⣶⣖⠢⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇");
-            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇");
-
+            Console.WriteLine("Работу выполнил студент группы 19ИЭ1БЗИ");
             Console.WriteLine("");
-            Console.WriteLine("┏━━┓┏┓┏┓┏━┓     ┏┓︱┏━┓┏━┓┏━━┓     ┏━━┓┏━━┓┏━━┓┏┳┓");
-            Console.WriteLine("┗┓┏┛┃┗┛┃┃┳┛     ┃┃︱┃┳┛┃┳┛┗┓┏┛     ┗┓┏┛┃┏┓┃┃━━┫┃┏┛");
-            Console.WriteLine("︱┃┃︱┃┏┓┃┃┻┓     ┃┗┓┃┻┓┃┻┓︱┃┃︱     ︱┃┃︱┃┣┫┃┣━━┃┃┗┓");
-            Console.WriteLine("︱┗┛︱┗┛┗┛┗━┛     ┗━┛┗━┛┗━┛︱┗┛︱     ︱┗┛︱┗┛┗┛┗━━┛┗┻┛");
+            Console.WriteLine("Иванов Евгений Андреевич");
+            Console.WriteLine("https://github.com/NecrosPrime/--sharp");
+            Console.WriteLine("https://vk.com/0x000000000f");
+            Console.WriteLine("нажмите любую кнопку для открытия пасхалочки...");
+
             Console.ReadKey(true);
-            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Зачет пжалста";
+            Timer myTimer = new Timer(new TimerCallback(CallBack), null, 0, 400); //таймер для пасхалочки
+            Console.ReadLine();
         }
-            static void Main(string[] args)
+
+
+        static void Main(string[] args)
         {
-            Console.Title = "Лабораторная работа #2" ;
-            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.Title = "Лабораторная работа #2"; //меняем заголовок окна
+            Console.ForegroundColor = ConsoleColor.Cyan; //меняем цвет. думаю светло-синий на черном фоне смотрится лучше, чем черно-белый 
             Console.Clear();
             Console.WriteLine("вариант 1");
             Console.WriteLine("вариант 2 ");
+            Console.WriteLine("вариант 3");
+            Console.WriteLine("вариант 8");
             Console.WriteLine("вариант 16");
             Console.WriteLine();
-            Console.WriteLine("Об авторе"); //вызов функции нулем :) не мог оскорбить свое эго и не зафиксировать копирайт)))
+            Console.WriteLine("Об авторе (0)"); //вызов функции нулем :) не мог оскорбить свое эго и не зафиксировать копирайт)))
 
             string menu = Console.ReadLine();
+            NewMethod1(args, menu);
+        }
+
+        private static void NewMethod1(string[] args, string menu)
+        {
             switch (menu)
-           {
-                case "1":
-                      V1(args);
+            {
+                case "1":           //первые 3 варианта
+                    V1(args);
                     break;
 
                 case "2":
@@ -184,32 +228,32 @@ namespace ConsoleApplication1
                     break;
 
                 case "3":
-                    NewMethod(args);
+                    V3(args);
                     break;
 
-                case "8":
+                case "8":           //мой вариант
                     V8(args);
                     break;
 
-                case "16":
+                case "16":          //рандомный вариант
                     V16(args);
                     break;
-                
-                case "0":
+
+                case "0":           //вызов функции об авторе
                     autor(args);
                     break;
-                case "1337":
-                    leet(args);
-                    break;
-            }
-        }
+                    /*case "1337":       //здесь должна быть пасхалочка, но пожалуй не буду.
+                        leet(args);
+                       break;
+                       */
 
-        private static void NewMethod(string[] args)
-        {
-            V3(args);
+            }
         }
     }
 }
+
+
+
 
 
 /*
@@ -229,4 +273,59 @@ namespace ConsoleApplication1
  * ＼二つ
  * 
  * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  static void leet(string[] args)
+          {
+
+              Console.OutputEncoding = System.Text.Encoding.GetEncoding(65001); //Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(" █████████████████████████ ");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇");
+            Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇");
+
+            Console.WriteLine("");
+              Console.WriteLine("┏━━┓┏┓┏┓┏━┓     ┏┓︱┏━┓┏━┓┏━━┓     ┏━━┓┏━━┓┏━━┓┏┳┓");
+              Console.WriteLine("┗┓┏┛┃┗┛┃┃┳┛     ┃┃︱┃┳┛┃┳┛┗┓┏┛     ┗┓┏┛┃┏┓┃┃━━┫┃┏┛");
+              Console.WriteLine("︱┃┃︱┃┏┓┃┃┻┓     ┃┗┓┃┻┓┃┻┓︱┃┃︱     ︱┃┃︱┃┣┫┃┣━━┃┃┗┓");
+              Console.WriteLine("︱┗┛︱┗┛┗┛┗━┛     ┗━┛┗━┛┗━┛︱┗┛︱     ︱┗┛︱┗┛┗┛┗━━┛┗┻┛");
+              Console.ReadKey(true);
+              Console.OutputEncoding = Encoding.UTF8;
+          }
  */
